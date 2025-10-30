e4a4b171f8219d24c8259fa140a2854bbdceb42e
yoTryme <jli758@ucsc.edu>
2025-10-29 22:35:48 -0700

    add image

--- ScrubDocuments/scrubableImage.js
+++ ScrubDocuments/scrubableImage.js
@@ -22,8 +22,8 @@ class ScrubImage {
         let w = 0;
         let h = 0;
         for (let region of this.regions){
-            w += region.x0 - region.x1;
-            h += region.y0 - region.y1;
+        w += Math.abs(region.x1 - region.x0);
+        h += Math.abs(region.y1 - region.y0);
         }
         this.winThreshold = w*h;
     }
@@ -33,7 +33,8 @@ class ScrubImage {
             this.points.push({x: mState.x, y: mState.y, end: mState.p});
         else if (mState.d)
             for (let i = 0; i < this.points.length; i ++)
-                if (abs(this.points[i].x - mState.x) + abs(this.points[i].y - mState.y) < WEIGHT * 5){
+                if (Math.abs(this.points[i].x - mState.x) + Math.abs(this.points[i].y - mState.y) < WEIGHT * 5){
+
                     if (this.points[i + 1])
                         this.points[i + 1].end = 1;
                     this.points.splice(i,1);
@@ -46,12 +47,12 @@ class ScrubImage {
         //check if player censored region of image needed to be ceonsored
         for (let region of this.regions)
         for (let i of this.points){
-            sameAsOtherPoint = false;
+            let sameAsOtherPoint = false;
             for (let j of this.points){
                 if (i == j)
                     break;
                 //100 == 10 squared
-                sameAsOtherPoint += 100 > (pow(j.x - i.x, 2) + pow(j.y - i.y, 2));
+                sameAsOtherPoint += 100 > (Math.pow(j.x - i.x, 2) + Math.pow(j.y - i.y, 2));
                 //above might be slow as hell if so use this instead
                 //sameAsOtherPoint += 10 > abs(j.x - i.x) + abs(j.y - i.y);
             }
@@ -86,4 +87,55 @@ class Region {
         this.x1 = x1;
         this.y1 = y1;
     }
+}
+
+let currentImage;
+let mState = {p: 0, d: 0, x: 0, y: 0, button: 0};
+let img;
+
+function preload(){
+    img = loadImage('img/forest1.jpg'); 
+}
+
+function setup(){
+    createCanvas(img.width, img.height);
+    
+    currentImage = new ScrubImage({
+        image: img,
+        regions: [
+            new Region(100, 100, 300, 300) 
+        ]
+    });
+}
+
+function draw(){
+    background(255);
+    
+    fill(255, 0, 0, 50);
+    noStroke();
+    for (let region of currentImage.regions){
+        rect(region.x0, region.y0, region.x1 - region.x0, region.y1 - region.y0);
+    }
+    
+    currentImage.update();
+    currentImage.draw();
+    
+    fill(0);
+    noStroke();
+    textSize(16);
+    text("Left click + drag: Draw | Right click: Erase", 10, 20);
+    
+    mState.p = 0;
+}
+
+function mousePressed(ev){
+    mState = {p: 1, d: 1, x: mouseX, y: mouseY, button: ev.button};
+}
+
+function mouseDragged(ev){
+    mState = {p: mState.p, d: 1, x: mouseX, y: mouseY, button: mState.button};
+}
+
+function mouseReleased(){
+    mState.d = 0;
 }
