ab30fa3b28afa4d8470e8466e0e2d8893127d5ac
Xavier Austin <166980912+xavierAustin@users.noreply.github.com>
2025-10-29 17:19:41 -0700

    made the win threshold stuff unshitty and not stupid

--- ScrubDocuments/scrubableImage.js
+++ ScrubDocuments/scrubableImage.js
@@ -1,29 +1,43 @@
 //will very likely need to be high
-const WIN_THRESHOLD = 2000;
+const WIN_THRESHOLD_SCALAR = 0.06;
 
 class ScrubbableImage {
     constructor(){
+        //mutable
         this.points = [];
         this.regions = [];
         this.x = 0;
         this.y = 0;
         this.image = null;
+        
+        //unmutable
+        let w = 0;
+        let h = 0;
+        for (let region of this.regions){
+            w += region.x0 - region.x1;
+            h += region.y0 - region.y1;
+        }
+        winThreshold = w*h;
     }
     checkWin(){
         let score = 0;
         //check if player censored region of image needed to be ceonsored
         for (let region of this.regions)
-            for (let i of this.points)
-                if (i.x > region.x0 && i.y > region.y0 && i.x < region.x1 && i.y < region.y1)
-                    score ++;
-        for (let i of this.points)
+        for (let i of this.points){
+            sameAsOtherPoint = false;
             for (let j of this.points){
-                let hD = i.x - j.x; //horizontal distance
-                let vD = i.y - j.y; //vertical distance
-                //clumped points decrease score; neatly distributed points increase
-                score *= min((vD*vD+hD*hD)/100,2);
+                if (i == j)
+                    break;
+                //100 == 10 squared
+                sameAsOtherPoint += 100 > (pow(j.x - i.x, 2) + pow(j.y - i.y, 2));
+                //above might be slow as hell if so use this instead
+                //sameAsOtherPoint += 10 > abs(j.x - i.x) + abs(j.y - i.y);
             }
-        if (score > WIN_THRESHOLD)
+            if (i.x > region.x0 && i.y > region.y0 && i.x < region.x1 && i.y < region.y1
+                && !sameAsOtherPoint)
+                score ++;
+        }
+        if (score > WIN_THRESHOLD_SCALAR)
             ;// idk win or somn      
     }
     draw(){
@@ -50,4 +64,3 @@ class Region {
         this.y1 = y1;
     }
 }
-//
